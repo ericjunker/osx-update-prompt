0e244528bea06c55ea466aa80ee742f2d2affeea
Eric Junker <ejunker@ithaca.edu>
2018-03-01 12:51:31 -0500

    See if a restart is really needed

--- update.sh
+++ update.sh
@@ -18,16 +18,21 @@ fi
 
 
 
-#install all updates but will not restart
-softwareupdate -i -a
+#install all updates but will not restart, store output
+softwareupdate -i -a > log.txt
 
+#see if the restart is needed
+if grep -Fxq "You have installed one or more updates that requires that you restart your computer" log.txt
+	then
+		#change 3 to however many times users should be able to put off updating
+		for i in {3..0}; do
+			#echo "Welcome $i"
+			python reminder.py $i
 
-#change 3 to however many times users should be able to put off updating
-for i in {3..0}; do
-	#echo "Welcome $i"
-	python reminder.py $i
-
-	#however long in seconds the program should wait before prompting the user again
-	sleep 10
-done
-
+			#however long in seconds the program should wait before prompting the user again
+			sleep 10
+		done
+	else
+		echo "no restart needed"
+		exit 0
+fi
